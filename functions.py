40feb6a5de89b48f3d50bbafabd8bb10aeaa137a
sarthaksalunke <sarthakss21.pumba@gmail.com>
2024-07-04 10:26:52 +0530

    added comment to get_todos function

--- functions.py
+++ functions.py
@@ -1,7 +1,11 @@
 
 FILEPATH = "todo.txt"
 
+
 def get_todos(filepath = FILEPATH):
+    """
+    gets the list of tasks from the text file
+    """
     with open(filepath, 'r') as file_local:
         the_list = file_local.readlines()
     return the_list
